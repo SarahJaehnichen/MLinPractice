e8d5c2021ef9f12db766c2adae99a5e897a05f63
ML in Practice <breise@uni-osnabrueck.de>
2021-10-08 09:51:14 +0200

    add matthews correlation coefficient metric

--- code/classification.sh
+++ code/classification.sh
@@ -5,10 +5,10 @@ mkdir -p data/classification/
 
 # run feature extraction on training set (may need to fit extractors)
 echo "  training set"
-python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle  -f -s 42 -a -k -b -f_1 -f_2 -pr
+python -m code.classification.run_classifier data/dimensionality_reduction/training.pickle -e data/classification/classifier.pickle  -f -s 42 -a -k -b -f_1 -f_2 -pr -mcc
 
 # run feature extraction on validation set (with pre-fit extractors)
 echo "  validation set"
-python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle -a -k -b -f_1 -f_2
+python -m code.classification.run_classifier data/dimensionality_reduction/validation.pickle -i data/classification/classifier.pickle -a -k -b -f_1 -f_2 -mcc
 
 # don't touch the test set, yet, because that would ruin the final generalization experiment!
--- code/classification/run_classifier.py
+++ code/classification/run_classifier.py
@@ -10,7 +10,7 @@ Created on Wed Sep 29 14:23:48 2021
 
 import argparse, pickle
 from sklearn.dummy import DummyClassifier
-from sklearn.metrics import accuracy_score, cohen_kappa_score, balanced_accuracy_score, f1_score, fbeta_score, precision_recall_curve, auc
+from sklearn.metrics import accuracy_score, cohen_kappa_score, balanced_accuracy_score, f1_score, fbeta_score, precision_recall_curve, auc, matthews_corrcoef
 
 # setting up CLI
 parser = argparse.ArgumentParser(description = "Classifier")
@@ -25,6 +25,7 @@ parser.add_argument("-k", "--kappa", action = "store_true", help = "evaluate usi
 parser.add_argument("-b", "--balanced_accuracy", action = "store_true", help = "evaluate using balanced accuracy") 
 parser.add_argument("-f_1", "--f_1", action = "store_true", help = "evaluate using f1-score") 
 parser.add_argument("-f_2", "--f_2", action = "store_true", help = "evaluate using f2-score") 
+parser.add_argument("-mcc", "--matthews_corrcoef", action = "store_true", help = "evaluate using matthews correlation coefficient")
 parser.add_argument("-pr", "--prec_recall_auc", action = "store_true", help = "evaluate using precision-recall area under the curve")
 args = parser.parse_args()
 
@@ -70,6 +71,9 @@ if args.f_1:
 # f2-score, other variants possible by changing beta 
 if args.f_2:
     evaluation_metrics.append(("f2 score", fbeta_score(data["labels"], prediction, beta=2)))
+# matthews correlation coefficient
+if args.matthews_corrcoef:
+    evaluation_metrics.append(("matthews corr-coefficient", matthews_corrcoef(data["labels"], prediction)))
 # precision-recall area under the curve 
 if args.prec_recall_auc:
     # first calculation of prec-recall-curve, thresholds unimportant for further calculations
